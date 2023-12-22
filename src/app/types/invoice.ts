67bab4d3de16c1c015e9d8c80adf1acebb3233e8
Kyle Nazario <knazario@bitovi.com>
2023-12-22 12:32:27 -0500

    App types

---
+++ src/app/types/invoice.ts
@@ -0,0 +1,16 @@
+export type Invoice = {
+  id: number;
+  customerId: number;
+  discount?: number;
+  total: number;
+  date: string;
+  lineItems: Array<LineItem>;
+};
+
+export type LineItem = {
+  id: number;
+  invoiceId: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+};
