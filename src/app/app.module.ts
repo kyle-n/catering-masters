1146be46365c28bd29f50cb117211beef155a437
Kyle Nazario <knazario@bitovi.com>
2023-12-22 13:31:05 -0500

    Create page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './containers/home/home.component';
+import { CreateInvoicePageComponent } from './containers/create-invoice-page/create-invoice-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, CreateInvoicePageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/containers/create-invoice-page/create-invoice-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-invoice-page',
+  templateUrl: './create-invoice-page.component.html',
+  styleUrls: ['./create-invoice-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CreateInvoicePageComponent {}
