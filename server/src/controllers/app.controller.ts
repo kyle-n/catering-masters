a4d0122ed9688dc84569bc2fb9a0410a0a8eade8
Kyle Nazario <knazario@bitovi.com>
2023-12-27 11:29:58 -0500

    Returns invoices

--- server/src/controllers/app.controller.ts
+++ server/src/controllers/app.controller.ts
@@ -1,17 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
-import { CustomerListItem } from '@shared/types';
+import { CustomerListItem, InvoiceListItem } from '@shared/types';
 import { Observable, map } from 'rxjs';
 import { mapCustomerToCustomerListItem } from 'src/mappers/customer-list-item.mapper';
+import { mapInvoiceToInvoiceListItem } from 'src/mappers/invoice-list-item.mapper';
 import { CustomerService } from 'src/services/customer.service';
+import { InvoiceService } from 'src/services/invoice.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly customerService: CustomerService) {}
+  constructor(
+    private readonly customerService: CustomerService,
+    private readonly invoiceService: InvoiceService
+  ) {}
 
   @Get('customers')
   getCustomers(): Observable<CustomerListItem[]> {
     return this.customerService
       .getCustomers()
       .pipe(map(customers => customers.map(mapCustomerToCustomerListItem)));
+  }
+
+  @Get('invoices')
+  getInvoices(): Observable<InvoiceListItem[]> {
+    return this.invoiceService
+      .getInvoices()
+      .pipe(map(invoices => invoices.map(mapInvoiceToInvoiceListItem)));
   }
 }
---
+++ server/src/mappers/invoice-list-item.mapper.ts
@@ -0,0 +1,8 @@
+import { InvoiceListItem } from '@shared/types';
+import { Invoice } from 'src/types/invoice';
+
+export function mapInvoiceToInvoiceListItem(invoice: Invoice): InvoiceListItem {
+  return {
+    id: invoice.id
+  };
+}
