eb8bd0dba6ec2bce1d3266aadc3856919bee87be
Kyle Nazario <knazario@bitovi.com>
2023-12-27 11:04:50 -0500

    Serves Angular app on all routes except /api

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'dist', 'catering-masters'),
+      exclude: ['/api/.*']
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
